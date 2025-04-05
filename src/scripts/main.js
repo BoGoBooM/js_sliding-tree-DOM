38352eaef686bad32af5b77679caad3ae43baa57
BoGoBooM <myPikcha@gmail.com>
2025-04-05 18:58:14 +0300

    task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,27 @@
 'use strict';
 
-// write code here
+const head = document.querySelectorAll('li');
+
+head.forEach((li) => {
+  const nestedList = li.querySelector('ul');
+
+  if (nestedList) {
+    const childNodes = li.childNodes;
+
+    const childText = Array.from(childNodes).find(
+      (node) => node.nodeType === 3 && node.textContent.trim() !== '',
+    );
+
+    const wrap = document.createElement('span');
+
+    wrap.textContent = childText.textContent;
+
+    li.replaceChild(wrap, childText);
+
+    wrap.addEventListener('click', (e) => {
+      if (nestedList) {
+        nestedList.hidden = !nestedList.hidden;
+      }
+    });
+  }
+});
